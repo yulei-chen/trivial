41aa8e94d9763ba8d19e1b6c6822808ef2d63333
Yulei Chen <chenyuleiw@gmail.com>
2025-12-12 00:45:46 +0100

    get episode json from rss

--- app/components/Episodes.vue
+++ app/components/Episodes.vue
@@ -1,8 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const { episodes } = useEpisodes()
+</script>
 
 <template>
-  <div>
-    Component: Episodes
+  <div v-for="episode in episodes" :key="episode.title">
+    <span>EP{{ episode.episode.toString().padStart(2, '0') }}: </span><NuxtLink :to="episode.link">
+      {{ episode.title }}
+    </NuxtLink>
   </div>
 </template>
 
---
+++ app/pages/episodes/[id].vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const { getEpisode } = useEpisodes()
+
+const route = useRoute()
+// @ts-expect-error - fix this
+const episode = getEpisode(Number(route.params.id))
+</script>
+
+<template>
+  <div>
+    <div>
+      <h1><span>EP{{ episode?.episode.toString().padStart(2, '0') }}: </span>{{ episode?.title }}</h1>
+    </div>
+    <div>
+      <p>{{ episode?.description }}</p>
+    </div>
+    <div>
+      <audio :src="episode?.audioUrl" controls />
+    </div>
+  </div>
+</template>
